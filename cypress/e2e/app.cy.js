a3567fae5d9af84abcd9ffea18ed6366de6570bb
Gerfson Alves <gerfson17@hotmail.com>
2024-09-16 15:42:57 -0300

    Teste do carrinho

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Amazon', () => {
+  beforeEach('Visitar site', () => {
+    cy.visit('https://www.amazon.com.br/')
   })
+
+  it('Pesquisando, adiciona, verifica', () => {
+    cy.get('#nav-search').type('Xbox Series S')
+    cy.get('#nav-search-submit-button').click()
+    cy.get('[data-asin="B0C7VK8CT4"] > .sg-col-inner > .s-widget-container > [data-action="puis-card-container-declarative"] > [data-cy="asin-faceout-container"] > .a-spacing-base > [data-cy="image-container"] > .rush-component > .a-link-normal > .a-section > .s-image').click()
+    cy.get('#add-to-cart-button').click()
+    cy.get('#nav-cart').click()
+    cy.get('#sc-subtotal-label-buybox.a-size-medium.sc-number-of-items')
+      .should('be.visible')
+    cy.get('.a-size-medium.a-color-base.sc-price.sc-white-space-nowrap')
+      .should('be.visible')
+    cy.contains('R$ 3.079,00')
+      .should('be.visible')
+  })
+
+
 })
